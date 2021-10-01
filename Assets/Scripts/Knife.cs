9742f42c1209b8160e2c092e4dab07207a249d20
Rodrigo Caetani <rocaetani@gmail.com>
2021-10-01 03:46:26 -0300

    Adjust on knife movement

--- Assets/Scripts/Knife.cs
+++ Assets/Scripts/Knife.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Numerics;
 using Unity.Collections;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
 
 public class Knife : MonoBehaviour
@@ -15,10 +16,13 @@ public class Knife : MonoBehaviour
 
     public Transform PushPosition;
     
+    public Transform CenterOfMass;
+
     // Start is called before the first frame update
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        //_rigidbody.centerOfMass = CenterOfMass.position;
     }
 
     // Update is called once per frame
@@ -27,14 +31,11 @@ public class Knife : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Q))
         {
         
-            Debug.Log("Aplicou forca");
             Vector3 newTarget = new Vector3(0.5f,1,0);
             _rigidbody.velocity = Vector3.zero;
-            //_rigidbody.AddForceAtPosition(newTarget * Force, PushPosition.position, ForceMode.Impulse);
             _rigidbody.AddForce(newTarget * Force, ForceMode.Impulse);
             
-            _rigidbody.AddTorque(RotationForce * Vector3.back);
-            
+            _rigidbody.AddTorque(RotationForce * Vector3.back, ForceMode.Impulse);
         }
         
     }
